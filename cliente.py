40cbb66bff7a7dfb971debce9d000df94f2b758e
fernuked <valusanmartin11@gmail.com>
2025-10-17 16:50:22 -0300

    creé archivos de las distintas clases + main y defini los atributos en el archivo cliente

---
+++ cliente.py
@@ -0,0 +1,10 @@
+def __init__ (self, nombre: str, apellido: str, email: str, password: str, dni: str, telefono: str, ciudad: str, pais: str):
+   
+    self.nombre = nombre
+    self.apellido = apellido
+    self.dni = dni
+    self.email = email
+    self.password = password
+    self.telefono = telefono
+    self.ciudad = ciudad
+    self.pais = pais
